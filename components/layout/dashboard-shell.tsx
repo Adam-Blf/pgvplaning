a4660254278993793458eff475b598c01ede5a26
Adam-Blf <adam.blf@example.com>
2026-03-11 15:45:18 +0100

    fix: correction structure JSX menu hamburger (login mobile)

--- components/layout/dashboard-shell.tsx
+++ components/layout/dashboard-shell.tsx
@@ -218,7 +218,7 @@ export function DashboardShell({ children }: DashboardShellProps) {
                         )}
                       </nav>
                     </div>
-              </div>
+                  </div>
             </header>
       </div>
 
